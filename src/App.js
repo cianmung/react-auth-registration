94e857608bf254f1a3a1958bbf9d14518e608029
Hau Sian Mung <sianmung.hau@websparks.sg>
2023-01-13 17:52:03 +0800

    Added Authentication & Protected Routes Feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import Register from "./Register";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
+import Home from "./components/Home";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import RequiredAuth from "./components/RequiredAuth";
+import { Routes, Route } from "react-router-dom";
+
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
   return (
-    <main className="App">
-      <Register />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* to protect these routes */}
+        <Route element={<RequiredAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route element={<RequiredAuth allowedRoles={[ROLES.Editor]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+        <Route element={<RequiredAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+
+        <Route
+          element={<RequiredAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
+          <Route path="lounge" element={<Lounge />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
